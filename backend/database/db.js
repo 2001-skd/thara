bc4d04e295201dc3503ee6f37ad10502b40b7afd
2001-skd <isanthakumardass@gmail.com>
2024-12-17 15:56:59 +0530

    return spelling changed

--- backend/database/db.js
+++ backend/database/db.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err);
-    retrun;
+    return;
   }
   console.log("Connected to MySQL database.");
 });
